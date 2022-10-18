c0539608e1e57b41a0d7473b6a4e385ba8dea360
biplab37 <biplabmahato37@gmail.com>
2022-10-18 17:37:34 +0200

    some Lippmann Schwinger

--- src/LDequation.jl
+++ src/LDequation.jl
@@ -1,2 +1,17 @@
 ## Solve the Lippmann-Schwinger equation in separable potential approximation
 
+function propagator(Λ::Float64,n::Int64)
+    return p->p/(p^2+Λ^2)
+end
+
+# Create the matrix Iᵢⱼ
+function create_I_matrix(terms::Array{Function,1},propagator::Function,Λ::Float64,n::Int64)
+    I = zeros(n,n)
+    return I
+end
+
+## Solve the Lippmann-Schwinger equation 
+function LS(model::Model,massgap::Vector{Float64})
+
+end
+
