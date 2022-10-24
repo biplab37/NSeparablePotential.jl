eef46a3866ca08dde9bdd4bafc1ecacfcdacfdca
biplab37 <biplabmahato37@gmail.com>
2022-10-24 14:57:03 +0200

    minor fix

--- src/custom_types.jl
+++ src/custom_types.jl
@@ -1,13 +1,13 @@
-Base.@kwdef struct Model
-    pot::Function
-    param::Parameters
-    n::Int64
-end
-
 Base.@kwdef mutable struct Parameters
     σ::Float64 = 0.19
     Λ::Float64 = 1.0
     m0::Float64 = 0.0
+end
+
+Base.@kwdef struct Model
+    pot::Function
+    param::Parameters
+    n::Int64
 end
 
 Base.@kwdef struct ResultGap
